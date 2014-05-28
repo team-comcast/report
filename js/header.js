e0521c1e62360bd38001d582a3c5c3e16340b018
Jackie <jwttseng@ucdavis.edu>
2014-05-27 20:55:12 -0700

    CSS Works Now
    Fixed it. I'm silly.

--- js/header.js
+++ js/header.js
@@ -1,6 +1,5 @@
 document.write('\
 <link href="css/bootstrap.min.css" rel="stylesheet">\
-<link href="css/bootstrap-theme.min.css" rel="stylsheet">\
 <link href="css/custom.css" rel="stylesheet">\
 <link href="http://fonts.googleapis.com/css?family=PT+Sans|Bitter" rel="stylesheet" type="text/css">\
 \
@@ -17,22 +16,22 @@ document.write('\
         <span class="icon-bar"></span>\
         <span class="icon-bar"></span>\
       </button>\
-    <a class="navbar-brand" href="/report/">Comcast</a>\
-</div>\
+      <a class="navbar-brand" href="/report/">Comcast</a>\
+    </div>\
 \
     <!-- Collect the nav links, forms, and other content for toggling -->\
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
       <ul class="nav navbar-nav">\
         <li><a href="business-probs.html">Business Problems</a></li>\
         <li class="dropdown">\
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Root Cause Analyses <b class="caret"</b></a>\
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Root Cause Analyses <b class="caret"></b></a>\
           <ul class="dropdown-menu">\
             <li class="dropdown-header">Poor Customer Interactions</li>\
             <li><a href="fishbone1.html">Repeat Calls</a></li>\
             <li><a href="fishbone2.html">Long Hold Times</a></li>\
             <li class="divider"></li>\
-            <li class="dropdown-header">Consumption Model</li>\
-            <li><a href="fishbone3.html">Dissatisfaction of Model</a></li>\
+            <li class="dropdown-header">Poor Consumption Model</li>\
+            <li><a href="fishbone3.html">Dissatisfaction with Model</a></li>\
           </ul>\
         </li>\
         <li class="dropdown">\
