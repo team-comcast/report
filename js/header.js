629de029dbaff730442507f66ec9e06844779a69
Jackie <jwttseng@ucdavis.edu>
2014-05-27 20:16:45 -0700

    Header
    Update Header

--- js/header.js
+++ js/header.js
@@ -1,43 +1,4 @@
-var otherpages = '<a class="navbar-brand" href="../OpenMeInChrome.html">Comcast</a>\
-</div>\
-\
-    <!-- Collect the nav links, forms, and other content for toggling -->\
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
-      <ul class="nav navbar-nav">\
-      <li class="dropdown">\
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Current vs. Future <b class="caret"></b></a>\
-          <ul class="dropdown-menu">\
-            <li><a href="curr-future-state.html">Current and Future State</a></li>\
-            <li class="divider"></li>\
-            <li><a href="current-sysmap.html">Current State System Map</a></li>\
-            <li><a href="future-sysmap.html">Future State System Map</a></li>\
-          </ul>\
-        </li>\
-        <li><a href="scorecard.html">Balanced Scorecard</a></li>\
-      </ul>'
-
-var homepage = '<a class="navbar-brand" href="#">Comcast</a>\
-</div>\
-\
-    <!-- Collect the nav links, forms, and other content for toggling -->\
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
-      <ul class="nav navbar-nav">\
-        <li class="dropdown">\
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Current vs. Future <b class="caret"></b></a>\
-          <ul class="dropdown-menu">\
-            <li><a href="sourceCode/curr-future-state.html">Current and Future State</a></li>\
-            <li class="divider"></li>\
-            <li><a href="sourceCode/current-sysmap.html">Current State System Map</a></li>\
-            <li><a href="sourceCode/future-sysmap.html">Future State System Map</a></li>\
-          </ul>\
-        </li>\
-        <li><a href="sourceCode/scorecard.html">Balanced Scorecard</a></li>\
-      </ul>'
-
-        
-
-
-var first = '\
+document.write('\
 <link href="css/bootstrap.min.css" rel="stylesheet">\
 <link href="css/custom.css" rel="stylesheet">\
 <link href="http://fonts.googleapis.com/css?family=PT+Sans|Bitter" rel="stylesheet" type="text/css">\
@@ -54,9 +15,38 @@ var first = '\
         <span class="icon-bar"></span>\
         <span class="icon-bar"></span>\
         <span class="icon-bar"></span>\
-      </button>'
-
-var last = '</div><!-- /.navbar-collapse -->\
+      </button>\
+    <a class="navbar-brand" href="/">Comcast</a>\
+</div>\
+\
+    <!-- Collect the nav links, forms, and other content for toggling -->\
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+      <ul class="nav navbar-nav">\
+        <li><a href="business-probs.html">Business Problems</a></li>\
+        <li class="dropdown">\
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Root Cause Analyses <b class="caret"</b></a>\
+          <ul class="dropdown-menu">\
+            <li class="dropdown-header">Poor Customer Interactions</li>\
+            <li><a href="fishbone1.html">Repeat Calls</a></li>\
+            <li><a href="fishbone2.html">Long Hold Times</a></li>\
+            <li class="divider"></li>\
+            <li class="dropdown-header">Consumption Model</li>\
+            <li><a href="fishbone3.html">Dissatisfaction of Model</a></li>\
+          </ul>\
+        </li>\
+        <li class="dropdown">\
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Current vs. Future <b class="caret"></b></a>\
+          <ul class="dropdown-menu">\
+            <li><a href="curr-future-state.html">Current and Future State</a></li>\
+            <li class="divider"></li>\
+            <li><a href="current-sysmap.html">Current State System Map</a></li>\
+            <li><a href="future-sysmap.html">Future State System Map</a></li>\
+          </ul>\
+        </li>\
+        <li><a href="mental-models.html">Mental Models</a></li>\
+        <li><a href="scorecard.html">Balanced Scorecard</a></li>\
+      </ul>\
+    </div><!-- /.navbar-collapse -->\
   </div><!-- /.container-fluid -->\
 </nav>\
 \
@@ -65,11 +55,4 @@ var last = '</div><!-- /.navbar-collapse -->\
     <p>By Fritz-Patrick Atienza, Claire Loncarich, Tilan Karunanayake, Sean Raley, Jaclyn Tseng, and Farah Zaidi</p>\
   </div>\
 </nav>'
-
-var path = window.location.pathname;
-var pathArray = path.split('/');
-
-if (pathArray[pathArray.length-1] == 'OpenMeInChrome.html')
-  document.write(first + homepage + last);
-else
-  document.write(first + otherpages + last);
+);
