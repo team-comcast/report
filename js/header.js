583be2dff82d2679500899d895cab1bc8248f7ac
Jackie <jwttseng@ucdavis.edu>
2014-05-27 20:29:31 -0700

    Update CSS again
    Does this one work!?

--- js/header.js
+++ js/header.js
@@ -1,10 +1,11 @@
 document.write('\
 <link href="css/bootstrap.min.css" rel="stylesheet">\
+<link href="css/bootstrap-theme.min.css" rel="stylsheet">\
 <link href="css/custom.css" rel="stylesheet">\
 <link href="http://fonts.googleapis.com/css?family=PT+Sans|Bitter" rel="stylesheet" type="text/css">\
 \
-<script type="text/javascript" src = "js/jquery.min.js"></script>\
-<script type="text/javascript" src = "js/bootstrap.min.js"></script>\
+<script type="text/javascript" src="js/jquery.min.js"></script>\
+<script type="text/javascript" src="js/bootstrap.min.js"></script>\
 \
 <nav class="navbar navbar-default" role="navigation">\
   <div class="container-fluid">\
@@ -16,7 +17,7 @@ document.write('\
         <span class="icon-bar"></span>\
         <span class="icon-bar"></span>\
       </button>\
-    <a class="navbar-brand" href="/">Comcast</a>\
+    <a class="navbar-brand" href="/report/">Comcast</a>\
 </div>\
 \
     <!-- Collect the nav links, forms, and other content for toggling -->\
