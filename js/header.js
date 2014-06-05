bc58c5d5f68aedbacd326d2025870bdc477cd4be
Jackie <jwttseng@ucdavis.edu>
2014-06-05 08:16:34 -0700

    Big Fat Update
    I'm almost done~

--- js/header.js
+++ js/header.js
@@ -17,12 +17,13 @@ document.write('\
         <span class="icon-bar"></span>\
         <span class="icon-bar"></span>\
       </button>\
-      <a class="navbar-brand" href="/report/">Comcast</a>\
+      <a class="navbar-brand" href="index.html">Comcast</a>\
     </div>\
 \
     <!-- Collect the nav links, forms, and other content for toggling -->\
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
       <ul class="nav navbar-nav">\
+        <li><a href="context.html">Context</a></li>\
         <li><a href="business-probs.html">Business Problems</a></li>\
         <li class="dropdown">\
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Root Cause Analyses <b class="caret"></b></a>\
@@ -36,16 +37,21 @@ document.write('\
           </ul>\
         </li>\
         <li class="dropdown">\
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Current vs. Future <b class="caret"></b></a>\
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Frameworks <b class="caret"></b></a>\
           <ul class="dropdown-menu">\
-            <li><a href="curr-future-state.html">Current and Future State</a></li>\
+            <li><a href="mental-models.html">Mental Models</a></li>\
             <li class="divider"></li>\
+            <li><a href="scorecard.html">Balanced Scorecard</a></li>\
+            <li class="divider"></li>\
+            <li><a href="curr-future-state.html">Current Future State</a></li>\
+            <li class="divider"></li>\
+            <li class="dropdown-header">System Maps</li>\
             <li><a href="current-sysmap.html">Current State System Map</a></li>\
             <li><a href="future-sysmap.html">Future State System Map</a></li>\
+            <li class="divider"></li>\
+            <li><a href="clv.html">Customer Lifetime Value</a></li>\
           </ul>\
         </li>\
-        <li><a href="mental-models.html">Mental Models</a></li>\
-        <li><a href="scorecard.html">Balanced Scorecard</a></li>\
         <li><a href="recommendations.html">Recommendations</a></li>\
       </ul>\
     </div><!-- /.navbar-collapse -->\
@@ -57,3 +63,9 @@ document.write('\
     <p>By Fritz-Patrick Atienza, Claire Loncarich, Tilan Karunanayake, Sean Raley, Jaclyn Tseng, and Farah Zaidi</p>\
   </div>\
 </nav>');
+
+
+            // <li class="divider"></li>\
+            // <li class="dropdown-header">Consumption Model Tradeoffs</li>\
+            // <li><a href="current-tradeoffs.html">Current Tradeoffs</a></li>\
+            // <li><a href="future-tradeoffs.html">Improved Tradeoffs</a></li>\
---
+++ js/popover.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+     $('#one').popover({
+            placement: 'top'
+        });
+     $('#two').popover({
+            placement: 'top'
+        });
+     $('#three').popover({
+            placement: 'top'
+        });
+     $('#four').popover({
+            placement: 'top'
+        });
+     $('#five').popover({
+            placement: 'top'
+        });
+     $('#six').popover({
+            placement: 'top'
+        });
+     $('#seven').popover({
+            placement: 'top'
+        });
+     $('#eight').popover({
+            placement: 'top'
+        });
+     $('#nine').popover({
+            placement: 'top'
+        });
+     $('#ten').popover({
+            placement: 'top'
+        });
+     $('#eleven').popover({
+            placement: 'top'
+        });
+     $('#twelve').popover({
+            placement: 'top'
+        });
+     $('#thirteen').popover({
+            placement: 'top'
+        });
+     $('#fourteen').popover({
+            placement: 'top'
+        });
+     $('#fifteen').popover({
+            placement: 'top'
+        });
+     $('#sixteen').popover({
+            placement: 'top'
+        });
+     $('#seventeen').popover({
+            placement: 'top'
+        });
+     $('#eighteen').popover({
+            placement: 'top'
+        });
+     $('#nineteen').popover({
+            placement: 'top'
+        });
+     $('#twenty').popover({
+            placement: 'top'
+        });
+     $('#twentyone').popover({
+            placement: 'top'
+        });
+     $('#twentytwo').popover({
+            placement: 'top'
+        });
+     $('#twentythree').popover({
+            placement: 'top'
+        });
+});
